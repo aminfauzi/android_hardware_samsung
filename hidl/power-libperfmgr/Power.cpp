571b9290671ff408d2b4b334d03c35bcfea332d5
SupremMotu <francisd132435@gmail.com>
2021-04-01 19:16:55 -0700

    power-libperfmgr: remove Lineage perf profiles

--- hidl/power-libperfmgr/Power.cpp
+++ hidl/power-libperfmgr/Power.cpp
@@ -47,8 +47,6 @@ constexpr char kPowerHalStateProp[] = "vendor.powerhal.state";
 constexpr char kPowerHalAudioProp[] = "vendor.powerhal.audio";
 constexpr char kPowerHalInitProp[] = "vendor.powerhal.init";
 constexpr char kPowerHalRenderingProp[] = "vendor.powerhal.rendering";
-constexpr char kPowerHalProfileNumProp[] = "vendor.powerhal.perf_profiles";
-constexpr char kPowerHalProfileProp[] = "vendor.powerhal.perf_profile";
 constexpr char kPowerHalConfigPath[] = "/vendor/etc/powerhint.json";
 
 Power::Power()
@@ -57,10 +55,8 @@ Power::Power()
       mVRModeOn(false),
       mSustainedPerfModeOn(false),
       mCameraStreamingMode(false),
-      mReady(false),
       mDoubleTapEnabled(false),
-      mNumPerfProfiles(0),
-      mCurrentPerfProfile(PowerProfile::BALANCED) {
+      mReady(false) {
     mInitThread = std::thread([this]() {
         android::base::WaitForProperty(kPowerHalInitProp, "1");
         mHintManager = HintManager::GetFromJSON(kPowerHalConfigPath);
@@ -103,25 +99,6 @@ Power::Power()
             mHintManager->DoHint("EXPENSIVE_RENDERING");
         }
 
-        state = android::base::GetProperty(kPowerHalProfileProp, "");
-        if (state == "POWER_SAVE") {
-            ALOGI("Initialize with POWER_SAVE profile");
-            setProfile(PowerProfile::POWER_SAVE);
-            mCurrentPerfProfile = PowerProfile::POWER_SAVE;
-        } else if (state == "BIAS_POWER_SAVE") {
-            ALOGI("Initialize with BIAS_POWER_SAVE profile");
-            setProfile(PowerProfile::BIAS_POWER_SAVE);
-            mCurrentPerfProfile = PowerProfile::BIAS_POWER_SAVE;
-        } else if (state == "BIAS_PERFORMANCE") {
-            ALOGI("Initialize with BIAS_PERFORMANCE profile");
-            setProfile(PowerProfile::BIAS_PERFORMANCE);
-            mCurrentPerfProfile = PowerProfile::BIAS_PERFORMANCE;
-        } else if (state == "HIGH_PERFORMANCE") {
-            ALOGI("Initialize with HIGH_PERFORMANCE profile");
-            setProfile(PowerProfile::HIGH_PERFORMANCE);
-            mCurrentPerfProfile = PowerProfile::HIGH_PERFORMANCE;
-        }
-
         // Now start to take powerhint
         mReady.store(true);
         ALOGI("PowerHAL ready to process hints");
@@ -130,7 +107,6 @@ Power::Power()
         // is finished so update dt2w state here
 	switchDT2W(mDoubleTapEnabled);
     });
-    mNumPerfProfiles = android::base::GetIntProperty(kPowerHalProfileNumProp, 0);
     mInitThread.detach();
 }
 
@@ -143,50 +119,6 @@ Return<void> Power::updateHint(const char *hint, bool enable) {
     } else {
         mHintManager->EndHint(hint);
     }
-    return Void();
-}
-
-Return<void> Power::setProfile(PowerProfile profile) {
-    if (mCurrentPerfProfile == profile) {
-        return Void();
-    }
-
-    // End previous perf profile hints
-    switch (mCurrentPerfProfile) {
-        case PowerProfile::POWER_SAVE:
-            mHintManager->EndHint("PROFILE_POWER_SAVE");
-            break;
-        case PowerProfile::BIAS_POWER_SAVE:
-            mHintManager->EndHint("PROFILE_BIAS_POWER_SAVE");
-            break;
-        case PowerProfile::BIAS_PERFORMANCE:
-            mHintManager->EndHint("PROFILE_BIAS_PERFORMANCE");
-            break;
-        case PowerProfile::HIGH_PERFORMANCE:
-            mHintManager->EndHint("PROFILE_HIGH_PERFORMANCE");
-            break;
-        default:
-            break;
-    }
-
-    // Apply perf profile hints
-    switch (profile) {
-        case PowerProfile::POWER_SAVE:
-            mHintManager->DoHint("PROFILE_POWER_SAVE");
-            break;
-        case PowerProfile::BIAS_POWER_SAVE:
-            mHintManager->DoHint("PROFILE_BIAS_POWER_SAVE");
-            break;
-        case PowerProfile::BIAS_PERFORMANCE:
-            mHintManager->DoHint("PROFILE_BIAS_PERFORMANCE");
-            break;
-        case PowerProfile::HIGH_PERFORMANCE:
-            mHintManager->DoHint("PROFILE_HIGH_PERFORMANCE");
-            break;
-        default:
-            break;
-    }
-
     return Void();
 }
 
@@ -386,15 +318,6 @@ Return<void> Power::powerHintAsync_1_3(PowerHint_1_3 hint, int32_t data) {
         return Void();
     }
 
-    switch (static_cast<LineagePowerHint>(hint)) {
-        case LineagePowerHint::SET_PROFILE:
-            setProfile(static_cast<PowerProfile>(data));
-            mCurrentPerfProfile = static_cast<PowerProfile>(data);
-            return Void();
-        default:
-            break;
-    }
-
     if (hint == PowerHint_1_3::EXPENSIVE_RENDERING) {
         ATRACE_INT(android::hardware::power::V1_3::toString(hint).c_str(), data);
         if (mVRModeOn || mSustainedPerfModeOn) {
@@ -410,16 +333,6 @@ Return<void> Power::powerHintAsync_1_3(PowerHint_1_3 hint, int32_t data) {
         return powerHintAsync_1_2(static_cast<PowerHint_1_2>(hint), data);
     }
     return Void();
-}
-
-// Methods from ::vendor::lineage::power::V1_0::ILineagePower follow.
-Return<int32_t> Power::getFeature(LineageFeature feature) {
-    switch (feature) {
-        case LineageFeature::SUPPORTED_PROFILES:
-            return mNumPerfProfiles;
-        default:
-            return -1;
-    }
 }
 
 constexpr const char *boolToString(bool b) {
--- hidl/power-libperfmgr/Power.h
+++ hidl/power-libperfmgr/Power.h
@@ -28,8 +28,6 @@
 
 #include "InteractionHandler.h"
 
-#include <vendor/lineage/power/1.0/ILineagePower.h>
-
 namespace android {
 namespace hardware {
 namespace power {
@@ -46,20 +44,7 @@ using PowerHint_1_2 = ::android::hardware::power::V1_2::PowerHint;
 using PowerHint_1_3 = ::android::hardware::power::V1_3::PowerHint;
 using ::android::perfmgr::HintManager;
 
-using ::vendor::lineage::power::V1_0::ILineagePower;
-using ::vendor::lineage::power::V1_0::LineageFeature;
-using ::vendor::lineage::power::V1_0::LineagePowerHint;
-
-enum PowerProfile {
-    POWER_SAVE = 0,
-    BALANCED,
-    HIGH_PERFORMANCE,
-    BIAS_POWER_SAVE,
-    BIAS_PERFORMANCE,
-    MAX
-};
-
-class Power : public IPower, public ILineagePower {
+class Power : public IPower {
   public:
     // Methods from ::android::hardware::power::V1_0::IPower follow.
 
@@ -80,9 +65,6 @@ class Power : public IPower, public ILineagePower {
     // Methods from ::android::hardware::power::V1_3::IPower follow.
     Return<void> powerHintAsync_1_3(PowerHint_1_3 hint, int32_t data) override;
 
-    // Methods from ::vendor::lineage::power::V1_0::ILineagePower follow.
-    Return<int32_t> getFeature(LineageFeature feature) override;
-
     // Methods from ::android::hidl::base::V1_0::IBase follow.
     Return<void> debug(const hidl_handle &fd, const hidl_vec<hidl_string> &args) override;
 
@@ -97,11 +79,7 @@ class Power : public IPower, public ILineagePower {
 
     std::atomic<bool> mDoubleTapEnabled;
 
-    int32_t mNumPerfProfiles;
-    std::atomic<PowerProfile> mCurrentPerfProfile;
-
     Return<void> updateHint(const char *hint, bool enable);
-    Return<void> setProfile(PowerProfile profile);
     Return<void> switchDT2W(bool dt2w);
 };
 
